a8e75df290a45d48494b4df015036c6f5b59ef91
osoken <osoken.devel@outlook.jp>
2020-01-21 01:54:34 +0900

    add SQLAlchemy-related configs

--- httppp/config.py
+++ httppp/config.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import os
 
-DEBUG = False
+
+DEBUG = True
 HOST = '0.0.0.0'
 PORT = 8080
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
+
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    ),
+    'db.sqlite3'
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = False
